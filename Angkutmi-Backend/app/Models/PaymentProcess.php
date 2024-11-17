7ed66d9bf9f12f5a665d985363e7eeafd637670a
liwangivone <liwangivone@gmail.com>
2024-11-18 00:21:39 +0800

    model for payment process

--- Angkutmi-Backend/app/Models/PaymentProcess.php
+++ Angkutmi-Backend/app/Models/PaymentProcess.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PaymentProcess extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'trip_id',
+        'payment_id',
+        'payment_status'
+    ];
 }
