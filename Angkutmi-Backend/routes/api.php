56b5afeefd13abf88f8bf592762a0d8698bb9b65
liwangivone <liwangivone@gmail.com>
2024-12-08 23:42:56 +0800

    conflict anjai

--- Angkutmi-Backend/routes/api.php
+++ Angkutmi-Backend/routes/api.php
@@ -5,13 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\TripController;
-<<<<<<< HEAD
 use App\Http\Controllers\SubscriptionController;
 use L5Swagger\Http\Controllers\SwaggerController;
-=======
 use App\Http\Controllers\CouponController;
 use App\Http\Controllers\WheelOfFortuneController;
->>>>>>> 18e0c2c8ef5cbd1669fbeb8f1a4eb4193125864f
+
+Route::get('/docs', [SwaggerController::class, 'api'])->name('l5-swagger.api');
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -31,7 +30,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user', function(Request $request){
         return $request->user();
     });
-<<<<<<< HEAD
 });
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -39,7 +37,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/subscriptions', [SubscriptionController::class, 'createSubscription']); // Membuat langganan
     Route::post('/subscriptions/{id}/payment', [SubscriptionController::class, 'createPayment']); // Membuat pembayaran
 });
-=======
     Route::post('/coupons', [CouponController::class, 'store']);
     Route::get('/coupons', [CouponController::class, 'get']);
     Route::get('/coupons/inventory', [CouponController::class, 'getUserClaimedCoupons']);
@@ -51,7 +48,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
     
-}
 
-);
->>>>>>> 18e0c2c8ef5cbd1669fbeb8f1a4eb4193125864f
+
+
