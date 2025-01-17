2dc5407b4d59b033aa23f7f1f08386b554ab4f15
Andrey Hartawan Suwardi <144876604+KingPublic@users.noreply.github.com>
2025-01-17 10:35:49 +0800

    justcheck

--- Angkutmi-Backend/app/Http/Controllers/TripController.php
+++ Angkutmi-Backend/app/Http/Controllers/TripController.php
@@ -129,6 +129,7 @@ class TripController extends Controller
         if ($trip->vehicle_type !== $driver->vehicle->type) {
             return response()->json(['message' => 'You cannot accept this trip. Vehicle type mismatch.'], 403);
         }
+        return response()->json($request->driver_location);
     
         $request->validate([
             'driver_location' => 'required'
